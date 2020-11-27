cfbf94a0ffc9abd2163aa907d68a87bf19af334a
Alex-bensimon <64471842+Alex-bensimon@users.noreply.github.com>
2020-11-27 23:23:56 +0100

    update comment

--- tweet_scraping_scroll.py
+++ tweet_scraping_scroll.py
@@ -16,8 +16,11 @@ from selenium.webdriver.support import expected_conditions as EC
 import pandas as pd 
 import numpy as np
 
+# Creation of the arrays that will fill the the csv file
 content = np.array([])
 tw_name = np.array([])
+
+# Used to decide how much tweets we want to scrap
 nb_of_tweets = 0
 
 def get_driver():
@@ -48,17 +51,16 @@ while nb_of_tweets<100:
     #   print(e.text)
         
     tweet_content = driver.find_elements_by_css_selector("div[class='css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0']")
-    
     for e in tweet_content:
         print("Tweet content : "+e.text)
         nb_of_tweets = nb_of_tweets + 1
         content = np.append(content,e.text.replace("\n"," "))
         
+    # tweet names and @ are stored in the array tw_name but not used yet
     tweet_name = driver.find_elements_by_css_selector("a[class='css-4rbku5 css-18t94o4 css-1dbjc4n r-1loqt21 r-1wbh5a2 r-dnmrzs r-1ny4l3l']")
     for e in tweet_name:
         tw_name = np.append(tw_name,e.text)
         print(e.text)
-
 
     # Scroll down to bottom
     scroll(wait,1,2)
@@ -66,16 +68,19 @@ while nb_of_tweets<100:
     # Wait to load page
     time.sleep(SCROLL_PAUSE_TIME)
 
+# create the csv file named tweets.csv
 np.savetxt("tweets.csv", content, delimiter=",",fmt='%s',encoding="utf-8")
 
 
 #%%
 
+# NE T'OCCUPE PAS DE CA ENCORE C'EST UN BROUILLON
+
 import pandas as pd 
 
 df = pd.read_csv("tweets.csv", error_bad_lines=False)
 
-i = 1
+i = 0
 
 for txt in df:
     print(df[i])
