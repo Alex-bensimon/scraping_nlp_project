3b16b45b3e511ffd27b4adab281e719a96c5c5f4
Victor Henrio <pro.henrio@gmail.com>
2020-11-29 22:19:41 +0100

    Fix 2 first name

--- tweet_scraping_scroll.py
+++ tweet_scraping_scroll.py
@@ -44,7 +44,7 @@ wait = WebDriverWait(driver,15)
 SCROLL_PAUSE_TIME = 2
 
 
-while nb_of_tweets<100:
+while nb_of_tweets<5:
     
     #tweet_content = driver.find_elements_by_css_selector("article")
     #for e in tweet_content:
@@ -53,20 +53,22 @@ while nb_of_tweets<100:
     tweet_content = driver.find_elements_by_css_selector("div[class='css-901oao r-18jsvk2 r-1qd0xha r-a023e6 r-16dba41 r-ad9z0x r-bcqeeo r-bnwqim r-qvutc0']")
     for e in tweet_content:
         print("Tweet content : "+e.text)
-        nb_of_tweets = nb_of_tweets + 1
+        nb_of_tweets += 1
         content = np.append(content,e.text.replace("\n"," "))
         
     # tweet names and @ are stored in the array tw_name but not used yet
     tweet_name = driver.find_elements_by_css_selector("a[class='css-4rbku5 css-18t94o4 css-1dbjc4n r-1loqt21 r-1wbh5a2 r-dnmrzs r-1ny4l3l']")
     for e in tweet_name:
         tw_name = np.append(tw_name,e.text)
-        print(e.text)
+        print("Tweent Name :",e.text,"\n")
 
     # Scroll down to bottom
     scroll(wait,1,2)
     # Wait to load page
     time.sleep(SCROLL_PAUSE_TIME)
 
+# to delete the first two personnes that tweeter recommand :
+tw_name = tw_name[2:]
 
 # create the csv file named tweets.csv
 np.savetxt("tweets.csv", content, delimiter="\\",fmt='%s',encoding="utf-8")
