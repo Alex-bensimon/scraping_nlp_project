8d6c4aafc5f076c7f4083067c994bfafb2905415
Victor Henrio <pro.henrio@gmail.com>
2021-01-04 14:18:59 +0100

    benchmark main

--- main.py
+++ main.py
@@ -9,17 +9,21 @@ import nlp
 import tweet_scraping_scroll as tws
 import sentiment_analysis as sent_analy
 import pandas as pd
+import crypto_benchmark as cryp_ben
 
 
 
 def main():
     
     loc_file = tws.get_tweet_from_subject(10,"en","bitcoin",20,20,20)
-    print(loc_file)
+    print("\n Nom du fichier :",loc_file,"\n")
     df = pd.read_csv("data/" +loc_file, sep="\\", names=['Content'])
     clean = nlp.clean_df(df)
     tweet_list = sent_analy.get_sentence_from_array(clean)
     sent_analy.get_sentiment_analyse(tweet_list)
+    
+    ## Benchmark crypto
+    cryp_ben.benchmark(nb_crypto=5)
 
 
 
